29efb8c479a881d038bd3bef4ac8c7f7e7e00d05
Erik Högberg <erik@e.email>
2023-03-14 08:57:16 +0100

    hello-skript exempel

---
+++ Spelutveckling2023/Assets/Scripts/nytt/hello.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello : MonoBehaviour
+{
+    public int nummer;
+    public float flyttal;
+    public string text;
+    public UnityEngine.Events.UnityEvent OnHello;
+
+    void Start()
+    {
+        Hello();
+    }
+
+    public void Hello(){
+        Debug.Log("Hello, World!");
+        OnHello.Invoke();
+    }
+}
